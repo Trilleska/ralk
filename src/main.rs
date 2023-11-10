5efbc2b966dbc73ce6ff87e42b70e3e61bf61eed
Maxi <voynich@disroot.org>
2023-11-09 20:05:54 -0500

    add 'r' operator check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // RALK > MAIN.RS
 
 use std::env;
+use rand; // for RNG functionality
 
 fn main() {
     // create new variable 'args'
@@ -14,6 +15,11 @@ fn main() {
     // this statement lets Rust infer the type of
     // the variable. in this case, `operator`.
     let operator = &args[1];
+
+    if operator == "r" {
+        println!("rand invoked");
+        std::process::exit(0);
+    }
     // omitted &, as var references cannot be converted
     let num1: i64 = args[2].parse().unwrap();
     let num2: i64 = args[3].parse().unwrap();
