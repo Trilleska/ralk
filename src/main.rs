7740b971470797b7d43077eeb86d405f5455ff9a
Maxi <voynich@disroot.org>
2023-11-09 20:01:17 -0500

    V1.0.0

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+// RALK > MAIN.RS
+
+use std::env;
+
+fn main() {
+    // create new variable 'args'
+    // type: Vector (basically an array)
+    // env::args().collect will "collect"
+    // all of the arguments and put them in
+    // a nice vector (array) for us to use <3
+    // see also: https://doc.rust-lang.org/std/env/fn.args.html
+    let args: Vec<String> = env::args().collect();
+
+    // this statement lets Rust infer the type of
+    // the variable. in this case, `operator`.
+    let operator = &args[1];
+    // omitted &, as var references cannot be converted
+    let num1: i64 = args[2].parse().unwrap();
+    let num2: i64 = args[3].parse().unwrap();
+
+    if operator == "a" {
+        let sum: i64 = num1 + num2;
+        println!("your sum is: {sum}");
+    } else if operator == "s" {
+        let sum: i64 = num1 - num2;
+        println!("your sum is: {sum}");
+    } else if operator == "d" {
+        let sum: i64 = num1 / num2;
+        println!("your sum is {sum}");
+    } else if operator == "m" {
+        let sum: i64 = num1 * num2;
+        println!("your sum is {sum}");
+    } else {
+        println!(">~< operator {operator} unrecognized!!");
+    }
+}
